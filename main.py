74ccfaa00de50328d1a87ba4c7b7b62ea24f03ae
Shahzeb Khan <shahzeb4325@gmail.com>
2024-09-14 20:05:00 +0530

    Add files via upload
    uploaded Python file

---
+++ main.py
@@ -0,0 +1,44 @@
+# step 1 - install PyCharm community edition or VS Code to start this project
+# step 2 - you need to import the "random" package which is already installed in PyCharm and VS Code
+# step 3 - start coding
+
+
+import random
+
+user_wins = 0
+computer_wins = 0
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+#   rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
